8d392d465d1d53d38c8571f8ca59207dc07f84d9
Henrich Tanis <henrich@Henrichs-MacBook-Pro.local>
2022-12-23 17:00:30 -0500

    created a var for user password and edited some of the CSS

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// the generate password function is not defined so I must define it below
+
+function generatePassword() {
+
+  var userPass= prompt(" How many characters would you like your password to be? Pick a number from 7 - 129 ");
+
+}
+
+
+// Write password to the #password inputy 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
