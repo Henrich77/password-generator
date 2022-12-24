b450b319b32f767a7865838e0d9c308539843673
Henrich Tanis <henrich@Henrichs-MacBook-Pro.local>
2022-12-23 19:09:27 -0500

    modified JS added a function for numbers

--- script.js
+++ script.js
@@ -3,11 +3,44 @@ var generateBtn = document.querySelector("#generate");
 
 // the generate password function is not defined so I must define it below
 
+function number() {
+
+  var allNums = []; 
+
+  for( i = 8; i <= 129; i++){
+    allNums.push(i);
+  }
+  return allNums
+}
+
+var chooseNumber = number();
+
+// console.log(chooseNumber[5]);
+
 function generatePassword() {
 
-  var userPass= prompt(" How many characters would you like your password to be? Pick a number from 7 - 129 ");
+  var userPass = prompt("How many characters do you want your password to be? ");
 
+  userPass = parseInt(userPass);
+
+  if (userPass == chooseNumber[i]){
+    prompt(" do you want any special characters in your password");
+  }
+  else { 
+    alert(" your password needs to be between 7-129!");
+  }
+
+
+
+ 
+  
+  
 }
+
+
+
+
+
 
 
 // Write password to the #password inputy 
